79f934e09831c8f0f49e2d50a76523a4098fceb4
istrage1621@gmail.com <istrage1621@gmail.com>
2018-03-21 23:31:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,3 +14,9 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
+
+function sayHiToGrandma(string) {
+  var lowercase = 'hello!'
+  lowercase.toLowerCase() === lowercase
+  return "I can't hear you!"
+}
